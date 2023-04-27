ea5e17307284ebf451d38f38b24c29367924b943
gabriel.fonseca <gabriel1911260@gmail.com>
2023-04-27 14:03:00 -0300

    Day 03 - Project 03

---
+++ project-3/script.js
@@ -0,0 +1,7 @@
+open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('opened'))
+
+close.addEventListener('click', () => container.classList.remove('opened'))
